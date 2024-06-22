8622b4161d7fbcae63923196df7bd2661051d2b7
MariPeshko <marunapeshko@gmail.com>
2024-06-22 19:03:09 +0200

    upd_error fork

--- pipex_bonus_22.06/srcs/pipex_bonus.c
+++ pipex_bonus_22.06/srcs/pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mpeshko <mpeshko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/05 10:33:51 by gcollet           #+#    #+#             */
-/*   Updated: 2024/06/22 17:58:20 by mpeshko          ###   ########.fr       */
+/*   Updated: 2024/06/22 19:02:01 by mpeshko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	error_fork(void)
 {
 	perror("Fork Error");
-	exit(EXIT_FAILURE);
+	// exit(EXIT_FAILURE);
 }
 
 int	child_process(char *argv, char **envp, int read_from)
@@ -25,10 +25,17 @@ int	child_process(char *argv, char **envp, int read_from)
 
 	if (pipe(fd) == -1)
 		error();
-	fprintf(stderr, "child_process. new pipe\nfd[0]: %i, fd[1]: %i\n", fd[0], fd[1]);
+	fprintf(stderr, "child_process. new pipe\nfd[0]: %i, fd[1]: %i\n", fd[0],
+		fd[1]);
 	pid = fork();
 	if (pid == -1)
+	{
 		error_fork();
+		close(fd[0]);
+		close(read_from);
+		close(fd[1]);
+		return (-1);
+	}
 	if (pid == 0)
 	{
 		close(fd[0]);
@@ -44,7 +51,7 @@ int	child_process(char *argv, char **envp, int read_from)
 		close(read_from);
 		waitpid(pid, NULL, 0);
 	}
-	return(fd[0]);
+	return (fd[0]);
 }
 
 /* Function who make a child process that will read from the stdin with
@@ -82,7 +89,6 @@ void	here_doc(char *limiter, int argc)
 int	main(int argc, char **argv, char **envp)
 {
 	int	cmd;
-	//int	input_file;
 	int	write_to;
 	int	read_from;
 
@@ -94,21 +100,23 @@ int	main(int argc, char **argv, char **envp)
 		read_from = open_file(argv[1], 2);
 		fprintf(stderr, "write_to: %i\n", read_from);
 		dup2(read_from, STDIN_FILENO);
-		//fprintf(stderr, "input_file: %i\n", input_file);
 		close(read_from);
 		fprintf(stderr, "close read_from(%i)\n\n", read_from);
 	}
-	//read_from = input_file;
 	while (cmd < argc - 2)
 	{
 		fprintf(stderr, "read_from: %i\n", read_from);
 		read_from = child_process(argv[cmd++], envp, read_from);
+		if (read_from = -1)
+		{
+			close(write_to);
+			exit(EXIT_FAILURE);
+		}
 		fprintf(stderr, "child_process returns read_from: %i\n\n", read_from);
 	}
 	close(read_from);
 	fprintf(stderr, "close(%i)\n", read_from);
 	dup2(write_to, STDOUT_FILENO);
-	
 	close(write_to);
 	fprintf(stderr, "close(%i)\n", write_to);
 	execute(argv[argc - 2], envp);
